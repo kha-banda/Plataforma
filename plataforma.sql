b7c75c9069c6f83e48ee0daa2d56ac04ab3efa68
464lalosegura <jimenezseguraeduardo@gmail.com>
2024-09-23 16:30:17 -0600

    Agregue la conexion de la base de datos de mysql y modifique la pagina de inicio

---
+++ plataforma.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 23-09-2024 a las 21:18:16
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `plataforma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formulario`
+--
+
+CREATE TABLE IF NOT EXISTS `formulario` (
+  `id` int(11) unsigned NOT NULL,
+  `nombre_completo` varchar(25) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `numero_tienda` varchar(255) NOT NULL,
+  `direccion_tienda` varchar(255) NOT NULL,
+  `comunidad_tienda` int(11) NOT NULL,
+  `tipo_producto` varchar(100) NOT NULL,
+  `nombre_usuario` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='en esta base de datos se  utilizara para poder almacenar la informacion';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE IF NOT EXISTS `inventario` (
+  `producto_id` int(11) NOT NULL,
+  `nombre_producto` varchar(100) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_unitario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Esta tabla se utilizara para poder saber el registro de los productos del almace';
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
