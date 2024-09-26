d4e4f3dcf72d1cb054feb79b17e8426193b25942
464lalosegura <jimenezseguraeduardo@gmail.com>
2024-09-26 14:18:22 -0600

    actualize la base de datos

--- plataforma.sql
+++ plataforma.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 25-09-2024 a las 20:23:35
+-- Tiempo de generación: 26-09-2024 a las 20:03:04
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -23,62 +23,108 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `formulario`
+-- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE IF NOT EXISTS `formulario` (
-  `id` int(11) unsigned NOT NULL,
-  `nombre_completo` varchar(50) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `numero_tienda` int(11) NOT NULL,
-  `direccion_tienda` varchar(255) NOT NULL,
-  `tipo_producto` varchar(100) NOT NULL,
-  `nombre_usuario` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `password` (`password`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='en esta base de datos se  utilizara para poder almacenar la informacion';
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `inventario`
---
-
-CREATE TABLE IF NOT EXISTS `inventario` (
-  `producto_id` int(11) NOT NULL,
-  `nombre_producto` varchar(100) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `precio_unitario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Esta tabla se utilizara para poder saber el registro de los productos del almace';
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE IF NOT EXISTS `productos` (
-  `pro` int(11) NOT NULL,
-  `marca` varchar(100) NOT NULL,
-  `categoria` varchar(100) NOT NULL,
-  `precio_unitario` int(11) NOT NULL,
-  `nombre_producto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Esta tabla se utilizara para poder saber el registro de los productos del almace';
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tiendas comunitarias`
---
-
-CREATE TABLE IF NOT EXISTS `tiendas comunitarias` (
-  `numero_tienda` int(11) NOT NULL,
-  `direccion` varchar(100) NOT NULL,
-  `nombre_encargado` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE IF NOT EXISTS `direccion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `estado` varchar(50) NOT NULL,
+  `ciudad` varchar(50) NOT NULL,
+  `colonia` varchar(50) NOT NULL,
+  `cp` varchar(10) NOT NULL,
+  `calle` varchar(50) NOT NULL,
+  `numero` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_producto`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido_producto` (
+  `pedido_id` int(11) NOT NULL AUTO_INCREMENT,
+  `producto_id` int(11) NOT NULL,
+  `canitdad` int(11) NOT NULL,
+  PRIMARY KEY (`pedido_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `cantidad` int(100) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reclamo`
+--
+
+CREATE TABLE IF NOT EXISTS `reclamo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `direccion_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
