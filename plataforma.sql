2566c175bcd54af15eeb2dad40dd1ee9f4330910
464lalosegura <85083361+464lalosegura@users.noreply.github.com>
2024-10-03 11:57:12 -0600

    modificacion de la base de datos, (se agrego tablas)

--- plataforma.sql
+++ plataforma.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 30-09-2024 a las 19:51:48
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.4.3
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 03-10-2024 a las 17:22:40
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `plataforma`
@@ -23,11 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `administrador`
+--
+
+DROP TABLE IF EXISTS `administrador`;
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `categoria`
 --
 
+DROP TABLE IF EXISTS `categoria`;
 CREATE TABLE IF NOT EXISTS `categoria` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -70,8 +87,9 @@ INSERT INTO `categoria` (`id`, `nombre`) VALUES
 -- Estructura de tabla para la tabla `direccion`
 --
 
+DROP TABLE IF EXISTS `direccion`;
 CREATE TABLE IF NOT EXISTS `direccion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `estado` varchar(50) NOT NULL,
   `ciudad` varchar(50) NOT NULL,
   `colonia` varchar(50) NOT NULL,
@@ -79,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `direccion` (
   `calle` varchar(50) NOT NULL,
   `numero` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `direccion`
@@ -94,16 +112,36 @@ INSERT INTO `direccion` (`id`, `estado`, `ciudad`, `colonia`, `cp`, `calle`, `nu
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+DROP TABLE IF EXISTS `movimiento`;
+CREATE TABLE IF NOT EXISTS `movimiento` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tipo_movimiento` enum('entrada','salida') NOT NULL,
+  `fecha` date NOT NULL,
+  `cantidad` int NOT NULL,
+  `origen` varchar(100) DEFAULT NULL,
+  `destino` varchar(100) DEFAULT NULL,
+  `id_producto` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_movimiento_producto` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pedido`
 --
 
+DROP TABLE IF EXISTS `pedido`;
 CREATE TABLE IF NOT EXISTS `pedido` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
   `fecha` date NOT NULL,
   `status` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -111,12 +149,13 @@ CREATE TABLE IF NOT EXISTS `pedido` (
 -- Estructura de tabla para la tabla `pedido_producto`
 --
 
+DROP TABLE IF EXISTS `pedido_producto`;
 CREATE TABLE IF NOT EXISTS `pedido_producto` (
-  `pedido_id` int(11) NOT NULL AUTO_INCREMENT,
-  `producto_id` int(11) NOT NULL,
-  `canitdad` int(11) NOT NULL,
+  `pedido_id` int NOT NULL AUTO_INCREMENT,
+  `producto_id` int NOT NULL,
+  `canitdad` int NOT NULL,
   PRIMARY KEY (`pedido_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -124,23 +163,24 @@ CREATE TABLE IF NOT EXISTS `pedido_producto` (
 -- Estructura de tabla para la tabla `producto`
 --
 
+DROP TABLE IF EXISTS `producto`;
 CREATE TABLE IF NOT EXISTS `producto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `descripcion` text NOT NULL,
-  `cantidad` int(100) NOT NULL,
+  `cantidad` int NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `imagen` varchar(255) NOT NULL,
-  `categoria_id` int(11) NOT NULL,
+  `categoria_id` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `cantidad`, `precio`, `imagen`, `categoria_id`) VALUES
-(1, 'Aceite Vegetal comestible "La Patrona"', '', 2, '13.00', '', 1);
+(1, 'Aceite Vegetal comestible \"La Patrona\"', '', 2, '13.00', '', 1);
 
 -- --------------------------------------------------------
 
@@ -148,14 +188,49 @@ INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `cantidad`, `precio`, `im
 -- Estructura de tabla para la tabla `reclamo`
 --
 
+DROP TABLE IF EXISTS `reclamo`;
 CREATE TABLE IF NOT EXISTS `reclamo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `producto_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `producto_id` int NOT NULL,
   `fecha` date NOT NULL,
   `descripcion` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_pedido`
+--
+
+DROP TABLE IF EXISTS `registro_pedido`;
+CREATE TABLE IF NOT EXISTS `registro_pedido` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_tienda` int DEFAULT NULL,
+  `id_producto` int DEFAULT NULL,
+  `fecha_pedido` date NOT NULL,
+  `cantidad_solicitada` int NOT NULL,
+  `status_pedido` enum('pendiente','procesado','entregado') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pedido_tienda` (`id_tienda`),
+  KEY `fk_pedido_producto` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda`
+--
+
+DROP TABLE IF EXISTS `tienda`;
+CREATE TABLE IF NOT EXISTS `tienda` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -163,17 +238,18 @@ CREATE TABLE IF NOT EXISTS `reclamo` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `telefono` varchar(15) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
-  `direccion_id` int(11) NOT NULL,
+  `direccion_id` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `correo` (`correo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -182,6 +258,27 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 INSERT INTO `usuario` (`id`, `nombre`, `apellidos`, `correo`, `telefono`, `contraseña`, `direccion_id`) VALUES
 (1, 'pedro', 'martinez otiz', 'ortiz_@outlook.com', '2314567896', 'xvervsdfbvvb', 0),
 (2, 'pedro', 'martinez otiz', 'ortz_@outlook.com', '2314567896', 'xvervsdfbvvb', 2);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD CONSTRAINT `fk_movimiento_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`),
+  ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
+
+--
+-- Filtros para la tabla `registro_pedido`
+--
+ALTER TABLE `registro_pedido`
+  ADD CONSTRAINT `fk_pedido_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`),
+  ADD CONSTRAINT `fk_pedido_tienda` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`),
+  ADD CONSTRAINT `registro_pedido_ibfk_1` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`),
+  ADD CONSTRAINT `registro_pedido_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
